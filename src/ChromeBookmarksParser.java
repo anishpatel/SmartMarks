b05f2ae490ec42905f850a9c01e17af513f9891a
Anish Patel <anishpatel.github@gmail.com>
2013-02-27 02:48:49 -0500

    fix merging merging error of CBP

--- src/ChromeBookmarksParser.java
+++ src/ChromeBookmarksParser.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> master
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,17 +5,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-<<<<<<< HEAD
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
-
-=======
-//import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-
->>>>>>> master
 public class ChromeBookmarksParser
 {
 	private final List<String> urls = new ArrayList<String>();
@@ -28,7 +15,6 @@ public class ChromeBookmarksParser
 	private void recur_parse(JsonReader reader) throws IOException
 	{
 		while (reader.hasNext()) {
-<<<<<<< HEAD
 			JsonToken nextToken = reader.peek();
 			if (nextToken == JsonToken.NAME) {
 				String name = reader.nextName();
@@ -46,68 +32,18 @@ public class ChromeBookmarksParser
 				reader.endArray();
 			} else {
 				reader.skipValue();
-=======
-			if (reader.peek() == JsonToken.NAME) {
-				String name = reader.nextName();
-/*				if (name.equals("roots")) {
-					reader.beginObject();
-					
-					reader.endObject();
-				} else if (name.equals("bookmark_bar")) {
-					reader.beginObject();
-					recur_parse(reader); // recurse
-					reader.endObject();
-				} else if (name.equals("other")) {
-					reader.beginObject();
-					recur_parse(reader); // recurse
-					reader.endObject();
-				} else if (name.equals("synced")) {
-					reader.beginObject();
-					recur_parse(reader); // recurse
-					reader.endObject();
-				} else if (name.equals("children")) {
-					reader.beginArray();
-					reader.beginObject();
-					recur_parse(reader); // recurse
-					reader.endArray();
-				} else*/ if (name.equals("url")) {
-					String url = reader.nextString();
-					urls.add(url);
-				} else {
-					if (reader.peek() == JsonToken.STRING) {
-						reader.skipValue(); // avoid string values
-					}
-				}
-			} else if (reader.peek() == JsonToken.BEGIN_OBJECT) {
-				reader.beginObject();
-				recur_parse(reader); // recurse
-				reader.endObject();
-			} else if (reader.peek() == JsonToken.BEGIN_ARRAY) {
-				reader.beginArray();
-				recur_parse(reader); // recurse
-				reader.endArray();
-			} else {
-				reader.skipValue(); // avoid unhandled events
->>>>>>> master
 			}
 		}
 	}
 	
 	public void parse()
 	{
-<<<<<<< HEAD
 		JsonReader reader = null;
-		try {
-			reader = new JsonReader(new FileReader("Bookmarks"));
-=======
 		 try {
-			JsonReader reader = new JsonReader(new FileReader("Bookmarks"));
->>>>>>> master
+			reader = new JsonReader(new FileReader("Bookmarks"));
 			reader.beginObject();
 			recur_parse(reader);
 			reader.endObject();
-			reader.close();
-<<<<<<< HEAD
 		} catch (FileNotFoundException fnfe) {
 			fnfe.printStackTrace();
 		} catch (IOException ioe) {
@@ -121,13 +57,6 @@ public class ChromeBookmarksParser
 				}
 			}
 		}
-=======
-	     } catch (FileNotFoundException e) {
-			e.printStackTrace();
-	     } catch (IOException e) {
-			e.printStackTrace();
-	     }
->>>>>>> master
 	}
 	
 	public List<String> getUrls()
@@ -144,8 +73,4 @@ public class ChromeBookmarksParser
 			System.out.println(url);
 		}
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 }
